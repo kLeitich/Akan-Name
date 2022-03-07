6509a177c696fbc7b2c8bb565480a4c22196350d
kLeitich <kevleitich@gmail.com>
2022-03-07 05:48:20 +0300

    adding trial three and getting error of 404 not found

--- index.js
+++ index.js
@@ -1,54 +1,48 @@
-
 const nameError = document.getElementById('name-error');
-const bDate = document.getElementById('bDate').value;
-const gender = document.getElementById('gender').value;
-const fName = document.getElementById('fName').value;
-
-
 
 function validateName(){
     const fName = document.getElementById('fName').value;
 
-    if (fName.length == 0){
-        nameError.innerHTML= 
-        'Name is required';
+    if (fName==0){
+        nameError.innerHTML='Name is required';
         return false;
     }
     if (!fName.match(/^[a-zA-z]/)){
-        nameError.innerHTML= 
-        'Write name using alphabets';
+        nameError.innerHTML='Name is only character not Number';
         return false;
     }
-    nameError.innerHTML='valid';
-    return true;
-}
+        nameError.innerHTML='Valid';
+        return true;
 
-
-var akanMale =["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "kaw", "Kofi", "Kwame"];
-var akanFemale =["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"];
-
-var Akan;
-
-getAkan.addEventListener("submit", (g)=>
+}   
+getAkan.addEventListener("submit",function generate(a,b){
+    // e.preventDefault();
+    const userData = document.userform.value;
+    var bDate = userData.get("b-Date")
+    var gender =userData.get("g-Gender")
+    const akanName = document.getElementById("akanName");
+    // const bDate = document.getElementById("bDate").value;
     
+    var newDate = new Date(bDate); 
+    var Day = bDate.getDay()
     
+    var akanFemale = ["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"];
+    var akanMale = ["Kwasi", "Kwadwo","Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"];
 
-    {var newDate = new date(bDate)
-    var day = newDate.getDay()
-    if (bDate==""){
-        alert("Enter your birthday correctly");
-    }
-    if (gender==""){
-        alert("Choose your gender");
-    }
-    else{
-        if (gender===Male){
-            Container.innerHTML='fname'+" your Akan Name is ${akanMale[day]}";
+    if(bDate==""){
+        alert ("Input your date of birth");
+        
+    }else if(gender==""){
+        alert("Select one gender");
+    }else{
+        if(gender=="Male"){
+            akanName.innerHTML = `Your khan name is ${akanMale[Day]}`
         }else{
-            Container.innerHTML='fname'+" your Akan Name is ${akanFemale[day]}";
+            akanName.innerHTML = `Your khan name is ${akanFemale[Day]}`
         }
+ 
     }
-    }
+});
 
 
 
@@ -58,9 +52,3 @@ getAkan.addEventListener("submit", (g)=>
 
 
 
-
-
-
-
-
-)
